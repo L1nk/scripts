70b149dff9b79cd6be2549e89fe7bfab4353f6d0
root <root@ubuntu.(none)>
2013-05-27 10:33:52 -0700

    master [adding preliminary install scripts]

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+echo ">>>>>>>>>>  Updating existing packages... <<<<<<<<<<"
+
+sudo apt-get update 
+
+echo ">>>>>>>>>> Update complete. <<<<<<<<<<"
+
+echo ">>>>>>>>>> Installing vim... <<<<<<<<<<"
+
+sudo apt-get install vim
+
+echo ">>>>>>>>>> vim installed. <<<<<<<<<<"
+
+echo ">>>>>>>>>> Installing git... <<<<<<<<<< "
+
+sudo apt-get install git
+
+echo ">>>>>>>>>> git installed. <<<<<<<<<<"
+
+
